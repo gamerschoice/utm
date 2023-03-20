b9b1496adc2542985464d3aade2a6467021d60ba
Ian Leckey <ian.leckey@gmail.com>
2023-03-20 22:02:35 +0000

    ghost a-tag in nav-link component

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -9,6 +9,3 @@ $classes = ($active ?? false)
 <a {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
-<a href="#" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center rounded-md px-2 py-2 text-sm font-medium">
-
-  </a>
