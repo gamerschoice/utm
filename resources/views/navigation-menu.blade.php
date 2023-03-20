9ccb6de351f33e3ba2ba3b2c539a5c62cf4f3098
Ian Leckey <ian.leckey@gmail.com>
2023-03-20 22:00:27 +0000

    search component started

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -98,6 +98,28 @@
                 {{ __('Billing') }}
             </x-nav-link>
 
+            <x-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+
+                <svg class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                  </svg>
+                  
+                {{ __('Profile') }}
+            </x-nav-link>
+
+            @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+            <x-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+
+                <svg class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                </svg>
+                  
+                {{ __('Team') }}
+            </x-nav-link>
+            @endif
+              
+
+              
         </nav>
 
     </div>
@@ -114,17 +136,7 @@
         </button>
         <div class="flex flex-1 justify-between px-4">
             <div class="flex flex-1">
-                <form class="flex w-full lg:ml-0" action="#" method="GET">
-                    <label for="search-field" class="sr-only">Search</label>
-                    <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                        <input id="search-field" class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 focus:border-transparent focus:outline-none focus:ring-0 focus:placeholder:text-gray-400 sm:text-sm" placeholder="Search" type="search" name="search">
-                    </div>
-                </form>
+                <x-search />
             </div>
 
             <div class="ml-4 flex items-center lg:ml-6">
