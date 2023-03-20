fd1bbcf6a60d795d081055c6271238843ddfd9df
Ian Leckey <ian.leckey@gmail.com>
2023-03-20 10:21:23 +0000

    whoops didnt save change on domain model fml

---
+++ app/Models/Domain.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Scout\Searchable;
+
+class Domain extends Model
+{
+    use HasFactory, HasUuids, Searchable;
+
+    public $guarded = ['id'];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    public function toSearchableArray(): array
+    {
+        $array = $this->only('domain');
+ 
+        return $array;
+    }
+
+    public function links(): HasMany
+    {
+        return $this->hasMany(Link::class);
+    }
+
+    public function team(): HasOne
+    {
+        return $this->hasOne(Team::class);
+    }
+    
+}
