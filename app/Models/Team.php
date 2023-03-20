48e5efd90608813924f49fa13b29ced65ceb4bb7
Ian Leckey <ian.leckey@gmail.com>
2023-03-20 14:15:33 +0000

    hasOne, hasMany

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Jetstream\Events\TeamCreated;
 use Laravel\Jetstream\Events\TeamDeleted;
 use Laravel\Jetstream\Events\TeamUpdated;
