5550037d96052a8deffbe54feb4dacbfe1aa4c10
Andrew Clark <andrew@aplaceforeverything.co.uk>
2023-03-21 09:35:33 +0000

    Don't show billing link if not a team owner

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function ownsCurrentTeam()
+    {
+        return $this->ownsTeam($this->currentTeam);
+    }
 }
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -66,14 +66,16 @@
                         </div>
                     </div>
         
-                    <a x-data="{ active: '{{ request()->routeIs('billing') }}' }" href="{{ route('billing') }}"
-                        :class="{ 'bg-gray-100 text-gray-900' : active, ' text-gray-600 hover:bg-gray-50 hover:text-gray-900' : !active }"
-                        class="group flex items-center rounded-md px-2 py-2 text-sm font-medium">
-                        <svg class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
-                        </svg>
-                        {{ __('Billing') }}
-                    </a>
+                    @if(auth()->user()->ownsCurrentTeam())
+                        <a x-data="{ active: '{{ request()->routeIs('billing') }}' }" href="{{ route('billing') }}"
+                            :class="{ 'bg-gray-100 text-gray-900' : active, ' text-gray-600 hover:bg-gray-50 hover:text-gray-900' : !active }"
+                            class="group flex items-center rounded-md px-2 py-2 text-sm font-medium">
+                            <svg class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                            </svg>
+                            {{ __('Billing') }}
+                        </a>
+                    @endif
         
                     <a x-data="{ active: '{{ request()->routeIs('profile.*') }}' }" href="{{ route('profile.show') }}"
                         :class="{ 'bg-gray-100 text-gray-900' : active, ' text-gray-600 hover:bg-gray-50 hover:text-gray-900' : !active }"
