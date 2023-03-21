56b98db4a4feaeaf5b2f69a86385a6aa2766df6b
Ian Leckey <ian.leckey@gmail.com>
2023-03-21 14:12:03 +0000

    domain count indicator

--- app/Http/Livewire/AppNavigationMenu.php
+++ app/Http/Livewire/AppNavigationMenu.php
@@ -10,10 +10,13 @@ class AppNavigationMenu extends \Laravel\Jetstream\Http\Livewire\NavigationMenu
 {
 
     public $domains;
+    public $totalDomains;
 
     public function mount()
     {
         $team_id = auth()->user()->currentTeam->id;
         $this->domains = Domain::where('team_id', $team_id)->take(10)->get();
+
+        $this->totalDomains = Domain::where('team_id', $team_id)->count();
     }
 }
