44f9f669254853db813675a3cd205f5278d362ac
Andrew Clark <andrew@aplaceforeverything.co.uk>
2023-03-21 09:40:43 +0000

    Add icons for invoice paid status

--- resources/views/livewire/billing/invoices.blade.php
+++ resources/views/livewire/billing/invoices.blade.php
@@ -25,7 +25,17 @@
                                                 <time datetime="2020-01-01">{{ $invoice->date()->toFormattedDateString() }}</time>
                                             </td>
                                             <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{{ $invoice->total() }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-500">{{ $invoice->isPaid() }}</td>
+                                            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                                                @if($invoice->isPaid())
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                    </svg>                                        
+                                                @else
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                                    </svg>
+                                                @endif
+                                            </td>
                                             <td class="whitespace-nowrap px-6 py-4 text-right text-sm font-medium">
                                                 <a href="{{ route('invoice', ['invoice' => $invoice->id])}}" class="text-blue-600 hover:text-blue-900">View invoice</a>
                                             </td>
