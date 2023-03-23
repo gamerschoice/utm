ee84b1e15702765a78cf1a2fb866ef2fdf46647e
Ian Leckey <ian.leckey@gmail.com>
2023-03-23 10:02:45 +0000

    bulk link creation notifications and customising. Refresh button on links list

--- app/Http/Livewire/Links/CreateAdvanced.php
+++ app/Http/Livewire/Links/CreateAdvanced.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Rules\MatchesConfiguredDomain;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Filament\Notifications\Notification; 
 
 class CreateAdvanced extends Component
 {
@@ -23,8 +24,26 @@ class CreateAdvanced extends Component
     public $presetMedium;
     public $presetCampaign;
 
+
+    public function resetData() 
+    {
+        $this->resetDestinationsLoaded();
+        $this->presetSource = '';
+        $this->presetMedium = '';
+        $this->presetCampaign = '';
+        $this->errorMessage = '';
+        $this->destinations = '';
+
+    }
+    public function resetDestinationsLoaded()
+    {
+        $this->destinationsLoaded = [];
+    }
+
     public function loadDestinations()
     {
+        $this->resetDestinationsLoaded();
+
         $split = preg_split('/[\r\n]+/', $this->destinations, -1, PREG_SPLIT_NO_EMPTY);
         $validator = Validator::make($split, [
             '*' => 'url',
@@ -37,16 +56,25 @@ class CreateAdvanced extends Component
         }
 
         foreach($validator->validated() as $url) {
-             $link = [];
-             $link['destination'] = $url;
-             $link['utm_source'] = $this->presetSource;
-             $link['utm_medium'] = $this->presetMedium;
-             $link['utm_campaign'] = $this->presetCampaign;
-             $this->destinationsLoaded[] = $link;
+            $link = [];
+            $link['destination'] = $url;
+            $link['utm_source'] = $this->presetSource;
+            $link['utm_medium'] = $this->presetMedium;
+            $link['utm_campaign'] = $this->presetCampaign;
+            $link['utm_term'] = '';
+            $link['utm_content'] = '';
+            $link['utm_source_platform'] = '';
+            $link['utm_creative_format'] = '';
+            $link['utm_marketing_tactic'] = '';
+            $link['notes'] = '';
+            $this->destinationsLoaded[] = $link;
         }
 
     }
 
+    /**
+     * @todo probably a much better way of doing this.
+     */
     public function importLinks()
     {
         foreach($this->destinationsLoaded as $link)
@@ -56,10 +84,25 @@ class CreateAdvanced extends Component
             $linkObj->utm_source = $link['utm_source'];
             $linkObj->utm_medium = $link['utm_medium'];
             $linkObj->utm_campaign = $link['utm_campaign'];
+            $linkObj->utm_term = $link['utm_term'];
+            $linkObj->utm_content = $link['utm_content'];
+            $linkObj->utm_source_platform = $link['utm_source_platform'];
+            $linkObj->utm_creative_format = $link['utm_creative_format'];
+            $linkObj->utm_marketing_tactic = $link['utm_marketing_tactic'];
+            $linkObj->notes = $link['notes'];
             $linkObj->domain_id = $this->domain_id;
             $linkObj->shortlink = Str::random(8);
-            $linkObj->save();
+            
         }
+
+        Notification::make() 
+            ->title('Links imported')
+            ->body('Your links have been imported successfully.')
+            ->success()
+            ->send(); 
+
+        $this->resetData();
+
     }
 
     public function mount(Request $request)
--- resources/views/livewire/links/advanced.blade.php
+++ resources/views/livewire/links/advanced.blade.php
@@ -13,7 +13,7 @@
                         <div><a @click.prevent="presetsOpen = !presetsOpen" rel="nofollow" class="cursor-pointer text-gray-900 font-semibold text-sm">Presets &raquo;</a></div>
                         <x-button wire:click.prevent="loadDestinations">
                             <span>Submit</span>
-                            <svg wire:loading class="ml-2 animate-spin inline-block h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <svg wire:loading wire:target="loadDestinations" class="ml-2 animate-spin inline-block h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
@@ -57,21 +57,91 @@
                 @if($errorMessage) <div class="text-red-500 font-semibold text-sm">{{ $errorMessage }}</div> @endif
                 @if(!empty($this->destinationsLoaded))
                     <div class="my-2 flex justify-end">
-                        <x-button wire:click.prevent="importLinks" type="button">Create links</x-button>
+                        <x-button wire:click.prevent="importLinks" wire:loading.attr="disabled" type="button">
+                            <span>Create links</span>
+                            <svg wire:loading wire:target="importLinks" class="ml-2 animate-spin inline-block h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                        </x-button>
                     </div>
                 @endif
-                @foreach($this->destinationsLoaded as $link)
-                    <div class="bg-gray-100 py-2 px-2 rounded-md shadow-sm my-2" x-data="{ open: false }">
+                @foreach($this->destinationsLoaded as $index => $link)
+                    <div class="bg-gray-100 py-3 px-3 rounded-md shadow-sm my-2" x-data="{ open: false }">
                         <div @click="open = !open" class="cursor-pointer flex justify-between items-center">
-                            <span class="text-gray-900 text-sm">{{ $link['destination'] }}</span>
+                            <span class="text-gray-900 text-sm font-medium">{{ $link['destination'] }}</span>
                             <span>
-                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-500">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                 </svg>
                             </span>                              
                         </div>
-                        <div x-cloak x-show="open">
-                            
+                        <div x-cloak x-show="open" class="border-t border-gray-300 mt-3 pt-3">
+                            <div class="grid sm:grid-cols-2 gap-x-4 gap-y-2">
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Source</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_source" />
+                                    </div>
+                                </div>
+            
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Medium</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_medium" />
+                                    </div>
+                                </div>
+                                
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Campaign</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_campaign" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Term</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_term" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Content</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_content" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Source platform</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_source_platform" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Creative format</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_creative_format" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm">
+                                    <x-label class="text-xs">Marketing tactic</x-label>
+                                    <div class="mt-1">
+                                        <x-input type="text" wire:model.defer="destinationsLoaded.{{ $index }}.utm_marketing_tactic" />
+                                    </div>
+                                </div>
+
+                                <div class="text-sm col-span-2">
+                                    <x-label class="text-xs">Notes</x-label>
+                                    <div class="mt-1">
+                                        <x-textarea type="text" wire:model.defer="destinationsLoaded.{{ $index }}.notes"></x-textarea>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 @endforeach
