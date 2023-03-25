dc013fd5b0f2e432cda6c81c8e94fe82add60e9c
Ian Leckey <ian.leckey@gmail.com>
2023-03-25 10:56:18 +0000

    updated domain card status label styling;

--- resources/views/components/domain-card.blade.php
+++ resources/views/components/domain-card.blade.php
@@ -7,16 +7,16 @@
         <h3 class="text-gray-900 font-bold text-xl truncate"><a href="{{ route('domain.view', $domain) }}">{{ $domain->domain }}</a></h3>
         
         @if($domain->dns_configured === 1)
-            <div class="shrink-0 flex gap-x-2 items-center bg-green-500 px-2 py-1 rounded-md border border-green-400">
-                <span class="cursor-default text-xs text-white font-light">{{ $domain->shortlink_domain }}</span>
-                <svg class="inline-block w-4 h-4 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <div class="shrink-0 flex gap-x-1 items-center cursor-default  rounded-full bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800">
+                <span>{{ $domain->shortlink_domain }}</span>
+                <svg class="inline-block w-3 h-3 text-green-800" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                 </svg>
             </div>
         @else 
-            <div class="shrink-0 flex gap-x-2 items-center bg-red-500 px-2 py-1 rounded-md border border-red-400">
-                <span class="cursor-default text-xs text-red-50">no short domain</span>
-                <svg class=" inline-block w-4 h-4 text-red-50" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <div class="shrink-0 flex gap-x-1 items-center cursor-default rounded-full bg-yellow-100 px-2.5 py-0.5 text-xs font-medium text-yellow-800">
+                <span>no short domain</span>
+                <svg class=" inline-block w-3 h-3 text-yellow-800" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             </div>
