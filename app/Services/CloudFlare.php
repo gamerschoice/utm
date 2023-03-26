839c13e0fc6ee40127c977fd2fd1ac2f851127a8
Ian Leckey <ian.leckey@gmail.com>
2023-03-26 16:37:51 +0100

    cloudflare api

--- app/Services/CloudFlare.php
+++ app/Services/CloudFlare.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\Response;
+use Carbon\Carbon;
 
 class CloudFlare {
 
@@ -19,7 +20,57 @@ class CloudFlare {
 
     public function test() {
 
-        return $this->listCustomHostnames();
+        //return $this->createCustomHostname('go4.ethptc.com');
+
+        //return $this->cacheShortlink('https://go2.ethptc.com/XYZ2', 'https://utmwise.com/?utm_source=test&utm_medium=test');
+    }
+
+    public function createCustomHostname(string $hostname)
+    {
+        $url = $this->baseUrl . '/zones/' . $this->zone . '/custom_hostnames';
+
+        $payload = [
+            'hostname' => $hostname,
+            'ssl' => [
+                'bundle_method' => 'ubiquitous',
+                'method' => 'txt',
+                'type' => 'dv',
+                'settings' => [
+                    'ciphers' => ['ECDHE-RSA-AES128-GCM-SHA256', 'AES128-SHA'],
+                    'early_hints' => 'off',
+                    'http2' => 'on',
+                    'min_tls_version' => '1.0',
+                    'tls_1_3' => 'on'
+                ],
+                'wildcard' => false
+            ]
+        ];
+
+        $response = Http::withToken( $this->apiToken )
+            ->post( $url, $payload );
+
+        return $this->handleResponse( $response );
+    }
+
+    public function createWorkerRoute( string $hostname )
+    {
+        $url = $this->baseUrl . '/zones/' . $this->zone . '/workers/routes';
+        $response = Http::withToken( $this->apiToken )
+                        ->post( $url, [
+                            'pattern' => $hostname . '/*',
+                            'script' => 'utmwise-shortlinker'
+                        ]);
+
+        return $this->handleResponse( $response );
+    }
+
+    public function deleteWorkerRoute( string $worker_route_id )
+    {
+        $url = $this->baseUrl . '/zones/' . $this->zone . '/workers/routes/' . $worker_route_id;
+        $response = Http::withToken( $this->apiToken )
+                        ->delete( $url );
+
+        return $this->handleResponse( $response );
     }
 
     public function listCustomHostnames()
@@ -40,6 +91,42 @@ class CloudFlare {
 
         return $this->handleResponse( $response );
     }
+
+    /**
+     * deletes custom hostname from cloudflare and any issued certificates
+     */
+    public function deleteCustomHostname( string $identifier )
+    {
+        $url = $this->baseUrl . '/zones/' . $this->zone . '/custom_hostnames/' . $identifier;
+        $response = Http::withToken( $this->apiToken )
+                        ->delete( $url );
+
+        return $this->handleResponse( $response );
+    }
+
+
+
+    public function cacheShortlink( string $shortlink_url, string $destination_url )
+    {
+        $url = $this->baseUrl . '/accounts/' . config('services.cloudflare.accountId') . '/storage/kv/namespaces/' . env('CF_WORKER_KV_NAMESPACE') . '/values/' . urlencode($shortlink_url);
+        $response = Http::withToken( $this->apiToken )
+                        ->put( $url, [
+                            'metadata' => [ 'created_at' => Carbon::now() ],
+                            'value' => $destination_url
+                        ]);
+
+        return $this->handleResponse( $response );
+    }
+
+    public function deleteShortlink()
+    {
+        $url = $this->baseUrl . '/accounts/' . config('services.cloudflare.accountId') . '/storage/kv/namespaces/' . env('CF_WORKER_KV_NAMESPACE') . '/values/' . urlencode($shortlink_url);
+        $response = Http::withToken( $this->apiToken )
+                        ->delete( $url );
+
+        return $this->handleResponse( $response );
+    }
+
 
     protected function handleResponse( Response $response )
     {
