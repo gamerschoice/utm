39f1fc6bfa23c8bc33b1497b463bbefdda1bc93d
Andrew Clark <andrew@aplaceforeverything.co.uk>
2023-03-27 17:16:19 +0100

    Fix PSR4 compliance on ShortDomain model

--- app/Models/ShortDomain.php
+++ app/Models/ShortDomain.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Shortdomain extends Model
+class ShortDomain extends Model
 {
     use HasFactory;
 
