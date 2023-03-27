18ef2d7685f3efb34c9c84065d016a60156d7e58
Ian Leckey <ian.leckey@gmail.com>
2023-03-27 16:50:50 +0100

    frontend ui fixes

--- resources/views/livewire/domains/view/links.blade.php
+++ resources/views/livewire/domains/view/links.blade.php
@@ -181,7 +181,7 @@
                 <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
             <div x-show="qrImage" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:max-w-[520px] sm:mx-auto"
-            x-trap.inert.noscroll="show"
+            x-trap.inert.noscroll="qrImage"
             x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
--- resources/views/livewire/links/link-panel.blade.php
+++ resources/views/livewire/links/link-panel.blade.php
@@ -90,7 +90,7 @@
                                             </dd>
                                         </div>
                                         <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                                            <dt class="text-sm font-medium text-gray-500 w-full truncate">Desination</dt>
+                                            <dt class="text-sm font-medium text-gray-500">Desination</dt>
                                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                                 <a x-show="!editing" class="underline decoration-dotted" href="{{ $link->destination }}" target="_blank" rel="noopener nofollow">
                                                     {{ $link->destination }}
@@ -161,9 +161,13 @@
                                                 <span x-show="!editing">{!! $link->notes !!}</span>
                                                 <x-textarea x-show="editing" wire:model.defer="link.notes">{{ $link->notes }}</x-textarea>
                                             </dd>
-                                                     <dt class="text-sm font-medium text-gray-500">URL</dt>
-                                            <dd class="mt-1 text                                            <dt class="text-sm font-medium text-gray-500">URL</dt>
-                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                        </div>
+                                        <div x-show="!editing" class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                                            <dt class="text-sm font-medium text-gray-500">
+                                                URL
+                                            </dt>
+                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 flex flex-col items-start gap-3">
+                                                <span class="w-full truncate">{!! $link->auto_url !!}</span>
                                                 <x-button type="button" x-data="{ copied: false }" class="text-xs" @click.prevent="window.navigator.clipboard.writeText('{{ $link->auto_url }}'); copied = true; setTimeout( function() { copied = false }, 3000);">
                                                     <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer mr-1 text-white-800">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
@@ -171,7 +175,36 @@
                                                     <span x-show="!copied">Copy</span>
                                                     <span x-cloak x-show="copied">Copied!</span>
                                                 </x-button>
-                                   </a>
+                                            </dd>
+                                        </div>
+                                        <div x-show="!editing" class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                                            <dt class="text-sm font-medium text-gray-500">
+                                                URL (long)
+                                            </dt>
+                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 flex flex-col items-start gap-3">
+                                                <span class="truncate w-full">{!! $link->full_url !!}</span>
+                                                <x-button-secondary type="button" x-data="{ copied: false }" class="text-xs" @click.prevent="window.navigator.clipboard.writeText('{{ $link->full_url }}'); copied = true; setTimeout( function() { copied = false }, 3000);">
+                                                    <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer mr-1 text-white-800">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                                                    </svg>
+                                                    <span x-show="!copied">Copy</span>
+                                                    <span x-cloak x-show="copied">Copied!</span>
+                                                </x-button-secondary>
+                                            </dd>
+                                        </div>
+                                        <div x-show="!editing" class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6" x-data="{ qrImage: QRCode.toDataURL('{{ $link->auto_url }}', { width:2560, quality:1 }) }">
+                                            <dt class="text-sm font-medium text-gray-500">
+                                                QR
+                                            </dt>
+                                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 flex flex-col items-start">
+                                                <div>
+                                                    <img x-bind:src="qrImage" class="w-full">
+                                                </div>
+                                                <a x-bind:href="qrImage" target="_blank" rel="noopener nofollow" download="qr.jpg">
+                                                    <x-button type="button">
+                                                        Download
+                                                    </x-button>
+                                                </a>
                                             </dd>
                                         </div>
                                     </dl>
