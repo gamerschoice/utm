03ece2c66a7b5dfb247b20fb0d67b5561b7a9faa
Ian Leckey <ian.leckey@gmail.com>
2023-03-29 11:02:46 +0100

    fix plan available query to remove trial plan from choices

--- app/Queries/PlanQueries.php
+++ app/Queries/PlanQueries.php
@@ -8,11 +8,11 @@ use App\Models\Team;
 class PlanQueries extends Builder
 {
     public function available(Team $team = null)
-    {
+    {   
         if($team) {
-            return $this->whereNot('id', $team->plan->id)->whereNot('sku', 'trial')->where('active', true);
+            return $this->whereNot('id', $team->plan->id)->where('active', true);
         }
 
-        return $this->where('active', true);
+        return $this->whereNot('sku', 'trial')->where('active', true);
     }
 }
