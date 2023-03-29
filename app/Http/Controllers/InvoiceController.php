03c5943161006f69b71e38fe2739efe9770969c1
Ian Leckey <ian.leckey@gmail.com>
2023-03-29 22:49:55 +0100

    add web url and email to invoice, fix download filename #63

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -8,6 +8,11 @@ class InvoiceController extends Controller
 {
     public function __invoke(Request $request, $invoice)
     {
-        return $request->user()->currentTeam->downloadInvoice($invoice);
+        return $request->user()->currentTeam->downloadInvoice($invoice, [
+            'product' => 'UTM Wise subscription',
+            'email' => 'support@utmwise.com',
+            'url' => 'https://utmwise.com',
+            'recipient' => 'test'
+        ]);
     }
 }
