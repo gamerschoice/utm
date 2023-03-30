038d97d3ce9a7a56f8218a8d48cac0a75382c0f8
Ian Leckey <ian.leckey@gmail.com>
2023-03-30 14:26:41 +0100

    added plan_expired col to team table, mastercard logo

--- database/migrations/2020_05_21_100000_create_teams_table.php
+++ database/migrations/2020_05_21_100000_create_teams_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('pm_type')->nullable();
             $table->string('pm_last_four', 4)->nullable();
             $table->timestamp('trial_ends_at')->nullable();
+            $table->boolean('plan_expired')->default(false);
             $table->timestamps();
         });
     }
