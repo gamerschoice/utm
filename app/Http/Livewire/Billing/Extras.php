dad27f71fc7346413d157682f9b358eed1351a52
Ian Leckey <ian.leckey@gmail.com>
2023-03-30 19:02:20 +0100

    add and remove extra domains and extra seats plans

--- app/Http/Livewire/Billing/Extras.php
+++ app/Http/Livewire/Billing/Extras.php
@@ -11,8 +11,118 @@ class Extras extends Component
 
     public $team;
 
+    public $domain_quantity;
+    public $seat_quantity;
+
     public function mount()
     {
+        $this->domain_quantity = 1;
+        $this->seat_quantity = 1;
+    }
+
+    public function addExtraDomains()
+    {
+        $added = $this->team->newSubscription('extra_domains', 'price_1MrPH6HwvhcWXVfoukyWcAgZ')
+            ->quantity( $this->domain_quantity )
+            ->create();
+
+        if(!$added) {
+            Notification::make() 
+            ->title('Subscription failed')
+            ->body('Sorry! We couldn\'t add your subscription at this time. Please try again later. If the problem persists, please reach out to support.')
+            ->danger()
+            ->send(); 
+            return false;
+        }
+            
+
+        $this->team->maximum_domains += $this->domain_quantity;
+        $this->team->save();
+
+        Notification::make() 
+            ->title('Subscription added')
+            ->body('Thank you for subscribing! Your subscription plan is now active')
+            ->success()
+            ->send(); 
+
+        $this->emit('$refresh');
+
+    }
+
+    public function cancelExtraDomains()
+    {
+
+        /**
+         * @todo check if user can reduce domains by $quantity_extra
+         */
+
+        $quantity_extra = $this->team->subscription('extra_domains')->quantity();
+        $cancelled = $this->team->subscription('extra_domains')->cancel();
+        if($cancelled) {
+            $this->team->maximum_domains -= $quantity_extra;
+            $this->team->save();
+
+            Notification::make() 
+                ->title('Extra domain subscription cancelled')
+                ->success()
+                ->send(); 
+
+        }
+
+        $this->emit('$refresh');
+
+    }
+
+
+    public function cancelExtraSeats()
+    {
+
+        /**
+         * @todo check if user can reduce domains by $quantity_extra
+         */
+
+        $quantity_extra = $this->team->subscription('extra_seats')->quantity();
+        $cancelled = $this->team->subscription('extra_seats')->cancel();
+        if($cancelled) {
+            $this->team->maximum_members -= $quantity_extra;
+            $this->team->save();
+
+            Notification::make() 
+                ->title('Extra seats subscription cancelled')
+                ->success()
+                ->send(); 
+
+        }
+
+        $this->emit('$refresh');
+
+    }
+
+    public function addExtraSeats()
+    {
+        $added = $this->team->newSubscription('extra_seats', 'price_1MrPHTHwvhcWXVfoGWqgeN11')
+            ->quantity( $this->seat_quantity )
+            ->create();
+
+        if(!$added) {
+            Notification::make() 
+            ->title('Subscription failed')
+            ->body('Sorry! We couldn\'t add your subscription at this time. Please try again later. If the problem persists, please reach out to support.')
+            ->danger()
+            ->send(); 
+            return false;
+        }
+
+        $this->team->maximum_members += $this->seat_quantity;
+        $this->team->save();
+
+        Notification::make() 
+            ->title('Subscription added')
+            ->body('Thank you for subscribing! Your subscription plan is now active')
+            ->success()
+            ->send(); 
+
+        $this->emit('$refresh');
 
     }
     
@@ -22,7 +132,9 @@ class Extras extends Component
 
         return view('livewire.billing.extras', [
             'team' => $this->team,
-            'subscription' => $this->team->subscription('default')
+            'subscription' => $this->team->subscription('default'),
+            'domain_extra_subscription' => $this->team->subscription('extra_domains'),
+            'seat_extra_subscription' => $this->team->subscription('extra_seats'),
         ]);
     }
 
--- resources/views/livewire/billing/extras.blade.php
+++ resources/views/livewire/billing/extras.blade.php
@@ -8,58 +8,121 @@
         <div class="mt-4">
             <ul role="list" class="-my-4 divide-y divide-gray-200 w-full">
 
-                <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: 1, product: '', cost_per: 5 }">
-                    <div class="flex items-center justify-between">
-                        <div class="min-w-0 flex items-center gap-2 select-none">
-                            <select x-model="quantity" id="quantity" name="quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>                
-                            <span class="text-sm font-medium text-gray-900">Additional domain<span x-show="quantity > 1">s</span></span>
-                            <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">5</span></span>
+                @if(!$domain_extra_subscription || !$domain_extra_subscription->active())
+                    <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: 1, key: 'price_1MrPH6HwvhcWXVfoukyWcAgZ', cost_per: 5 }">
+                        <div class="flex items-center justify-between">
+                            <div class="min-w-0 flex items-center gap-2 select-none">
+                                <select wire:model="domain_quantity" x-model="quantity" name="domain_quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>                
+                                <span class="text-sm font-medium text-gray-900">Additional domain<span x-show="quantity > 1">s</span></span>
+                                <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">5</span></span>
+                            </div>
+                            <div>
+                                <button @click="confirming = true" x-show="!confirming" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                    Add
+                                </button>
+                                <div x-show="confirming" x-cloak class="flex items-center gap-2">
+                                    <span class="text-xs font-medium">Are you sure?</span>
+                                    <button wire:click="addExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                        <span>Confirm</span>
+                                        <svg wire:loading wire:target="addExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <button @click="confirming = true" x-show="!confirming" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                Add
-                            </button>
-                            <div x-show="confirming" x-cloak class="flex items-center gap-2">
-                                <span class="text-xs font-medium">Are you sure?</span>
-                                <button type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                    Confirm
+                    </li>
+                @else
+                    <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: {{ $domain_extra_subscription->quantity }}, key: 'price_1MrPH6HwvhcWXVfoukyWcAgZ', cost_per: 5 }">
+                        <div class="flex items-center justify-between">
+                            <div class="min-w-0 flex items-center gap-2 select-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-blue-600">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                                </svg>
+                                  
+                                <select disabled="disabled" wire:model="domain_quantity" x-model="quantity" id="quantity" name="quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+                                    <option>{{ $domain_extra_subscription->quantity }}</option>
+                                </select>                
+                                <span class="text-sm font-medium text-gray-900">Additional domain<span x-show="quantity > 1">s</span></span>
+                                <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">5</span></span>
+                            </div>
+                            <div>
+                                <button wire:click="cancelExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                    <span>Remove</span>
+                                    <svg wire:loading wire:target="cancelExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
                                 </button>
                             </div>
                         </div>
-                    </div>
-                </li>
-                <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: 1, product: '', cost_per: 2 }">
+                    </li>
+                @endif
+
+                @if(!$seat_extra_subscription || !$seat_extra_subscription->active())
+                    <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: 1, cost_per: 2 }">
+                        <div class="flex items-center justify-between">
+                            <div class="min-w-0 flex items-center gap-2 select-none">
+                                <select wire:mode="seat_quantity" x-model="quantity" name="seat_quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>                
+                                <span class="text-sm font-medium text-gray-900">Additional seat<span x-show="quantity > 1">s</span></span>
+                                <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">2</span></span>
+                            </div>
+                            <div>
+                                <button @click="confirming = true" x-show="!confirming" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                    Add
+                                </button>
+                                <div x-show="confirming" x-cloak class="flex items-center gap-2">
+                                    <span class="text-xs font-medium">Are you sure?</span>
+                                    <button wire:click="addExtraSeats" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                        <span>Confirm</span>
+                                        <svg wire:loading wire:target="addExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                @else 
+                <li class="py-4 cursor-default" x-data="{ confirming: false, quantity: {{ $seat_extra_subscription->quantity }}, cost_per: 2 }">
                     <div class="flex items-center justify-between">
                         <div class="min-w-0 flex items-center gap-2 select-none">
-                            <select x-model="quantity" id="quantity" name="quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-blue-600">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                            </svg>
+                              
+                            <select disabled="disabled" name="seat_quantity" class="max-w-full rounded-md border border-gray-300 py-1 text-left text-sm font-medium leading-5 text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+                                <option>{{ $seat_extra_subscription->quantity }}</option>
                             </select>                
-                            <span class="text-sm font-medium text-gray-900">Additional user<span x-show="quantity > 1">s</span>/seat<span x-show="quantity > 1">s</span></span>
+                            <span class="text-sm font-medium text-gray-900">Additional seat<span x-show="quantity > 1">s</span></span>
                             <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">2</span></span>
                         </div>
                         <div>
-                            <button @click="confirming = true" x-show="!confirming" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                Add
+                            <button wire:click="cancelExtraSeats" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                <span>Remove</span>
+                                <svg wire:loading wire:target="cancelExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
                             </button>
-                            <div x-show="confirming" x-cloak class="flex items-center gap-2">
-                                <span class="text-xs font-medium">Are you sure?</span>
-                                <button type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                    Confirm
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </li>
+                @endif
             </ul>
         </div>
 
