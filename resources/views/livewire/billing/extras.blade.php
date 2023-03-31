fff3e64e78bdbae7c8e06d26f4b27313bbdc1243
Ian Leckey <ian.leckey@gmail.com>
2023-03-31 02:35:30 +0100

    fixed loading spinner colors on extra sub add/remove

--- resources/views/livewire/billing/extras.blade.php
+++ resources/views/livewire/billing/extras.blade.php
@@ -30,7 +30,7 @@
                                     <span class="text-xs font-medium">Are you sure?</span>
                                     <button wire:click="addExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                                         <span>Confirm</span>
-                                        <svg wire:loading wire:target="addExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <svg wire:loading wire:target="addExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                         </svg>
@@ -56,7 +56,7 @@
                             <div>
                                 <button wire:click="cancelExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                                     <span>Remove</span>
-                                    <svg wire:loading wire:target="cancelExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <svg wire:loading wire:target="cancelExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                     </svg>
@@ -88,7 +88,7 @@
                                     <span class="text-xs font-medium">Are you sure?</span>
                                     <button wire:click="addExtraSeats" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                                         <span>Confirm</span>
-                                        <svg wire:loading wire:target="addExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <svg wire:loading wire:target="addExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                         </svg>
@@ -114,7 +114,7 @@
                         <div>
                             <button wire:click="cancelExtraSeats" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                                 <span>Remove</span>
-                                <svg wire:loading wire:target="cancelExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <svg wire:loading wire:target="cancelExtraSeats" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
