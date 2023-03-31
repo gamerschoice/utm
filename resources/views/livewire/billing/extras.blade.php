3a9e73340258fa83e997d270828778a545db1c2e
Ian Leckey <ian.leckey@gmail.com>
2023-03-31 12:02:08 +0100

    label for additional sub ending closes #72

--- resources/views/livewire/billing/extras.blade.php
+++ resources/views/livewire/billing/extras.blade.php
@@ -54,13 +54,19 @@
                                 <span class="text-sm text-gray-600">£<span x-text="cost_per * quantity">5</span> /m</span>
                             </div>
                             <div>
-                                <button wire:click="cancelExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                    <span>Remove</span>
-                                    <svg wire:loading wire:target="cancelExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                    </svg>
-                                </button>
+                                @if($domain_extra_subscription->onGracePeriod())
+                                    <span class="inline-flex items-center rounded-full bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800">
+                                        Ending {{ $domain_extra_subscription->ends_at }}
+                                    </span>
+                                @else
+                                    <button wire:click="cancelExtraDomains" type="button" class="inline-flex items-center rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                        <span>Remove</span>
+                                        <svg wire:loading wire:target="cancelExtraDomains" class="ml-1 animate-spin inline-block h-3 w-3 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    </button>
+                                @endif
                             </div>
                         </div>
                     </li>
