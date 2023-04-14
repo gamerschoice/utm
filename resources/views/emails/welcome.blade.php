a738c8b66a7e2023b925026174dc4710700299ef
Ian Leckey <ian.leckey@gmail.com>
2023-04-14 22:48:07 +0100

    welcome email

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-mail::message>
+# Welcome to UTM Wise
+
+Thanks for signing up! Your account is now active and your free trial has commenced! 🎉
+
+Your trial will last for 7 days, and you can upgrade to a paid plan at any time. 
+
+<x-mail::button :url="route('dashboard')">
+Your Dashboard
+</x-mail::button>
+
+One thing to be aware of though - any short domains you set up will be removed upon the expiration of your trial *if* you don't upgrade to a paid plan. Likewise many of your account features will be inaccessible after this time.
+</x-mail::message>
+
