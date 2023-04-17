d459d89b97b5a5325c0451a654709a3b7c9d8a7a
Ian Leckey <ian.leckey@gmail.com>
2023-04-17 16:59:17 +0100

    wip

--- tests/Feature/StripeTest.php
+++ tests/Feature/StripeTest.php
@@ -8,27 +8,59 @@ use App\Listeners\StripeWebhookListener;
 
 class StripeTest extends TestCase
 {
-    protected array $payload = [
-        'data' => [
-            'object' => [
-                'customer' => 'cus_NeSqyPvxrNmitC',
-                'plan' => [
-                    'id' => 'price_1Mt9uiHwvhcWXVfovLNANFnD'
-                ],
-            ],
-        ],
-        'type' => 'customer.subscription.deleted',
-    ];
 
     public function test_extra_seat_cancellation(): void
     {
+        $payloadSeat = [
+            'data' => [
+                'object' => [
+                    'customer' => 'cus_NeSqyPvxrNmitC',
+                    'plan' => [
+                        'id' => env('STRIPE_SEAT_KEY')
+                    ],
+                ],
+            ],
+            'type' => 'customer.subscription.deleted',
+        ];
+        $listener = new StripeWebhookListener;
+        //$listener->cancelSeatExtra( $payloadSeat );
+        //assert something....
+
     }
 
     public function test_extra_domain_cancellation(): void
     {
+        $payloadDomain = [
+            'data' => [
+                'object' => [
+                    'customer' => 'cus_NeSqyPvxrNmitC',
+                    'plan' => [
+                        'id' => env('STRIPE_DOMAIN_KEY')
+                    ],
+                ],
+            ],
+            'type' => 'customer.subscription.deleted',
+        ];
+        $listener = new StripeWebhookListener;
+        //$listener->cancelDomainExtra( $payloadDomain );
+        //assert something....
     }
 
     public function test_plan_cancellation(): void 
     {
+        $payloadPlan = [
+            'data' => [
+                'object' => [
+                    'customer' => 'cus_NeSqyPvxrNmitC',
+                    'plan' => [
+                        'id' => 'price_1Mt9uiHwvhcWXVfovLNANFnD'
+                    ],
+                ],
+            ],
+            'type' => 'customer.subscription.deleted',
+        ];
+        $listener = new StripeWebhookListener;
+        //$listener->cancelPlan( $payloadPlan );
+        //assert something....
     }
 }
