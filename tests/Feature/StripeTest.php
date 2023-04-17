26396ae6c19077869a61691dee1bb2a83fd05ba6
Ian Leckey <ian.leckey@gmail.com>
2023-04-17 16:52:45 +0100

    start stripe webhook testing

---
+++ tests/Feature/StripeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Listeners\StripeWebhookListener;
+
+class StripeTest extends TestCase
+{
+    protected $event = new StripeWebhookListener;
+    protected array $payload = [
+        'data' => [
+            'object' => [
+                'customer' => 'cus_NeSqyPvxrNmitC',
+                'plan' => [
+                    'id' => 'price_1Mt9uiHwvhcWXVfovLNANFnD'
+                ],
+            ],
+        ],
+        'type' => 'customer.subscription.deleted',
+    ];
+
+    public function test_extra_seat_cancellation(): void
+    {
+    }
+
+    public function test_extra_domain_cancellation(): void
+    {
+    }
+
+    public function test_plan_cancellation(): void 
+    {
+    }
+}
